125029df751280ed9c34cfafa982173c3962e45c
Paul Andrew Santos <paulandrewsantos@gmail.com>
2025-01-22 14:11:30 +0800

    Handling Errors
    Chapter 13

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -12,6 +12,8 @@ const FormSchema = z.object({
     date: z.string(),
   });
 
+
+  //CREATE INVOICE 
   const CreateInvoice = FormSchema.omit({ id: true, date: true });
  
   export async function createInvoice(formData: FormData) {
@@ -31,3 +33,32 @@ const FormSchema = z.object({
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
   }
+
+  //EDIT INVOICE
+  const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+  
+  export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+//EDIT INVOICE
+
+  export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
+}
