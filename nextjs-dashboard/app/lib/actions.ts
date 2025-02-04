c3340bd74eeb18f9eb918d490c831f01e6e97f49
Paul Andrew Santos <paulandrewsantos@gmail.com>
2025-02-04 16:03:12 +0800

    Update actions.ts

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -24,11 +24,15 @@ const FormSchema = z.object({
     });
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
-
+try {
     await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+} catch (error) {
+  // We'll log the error to the console for now
+  console.error(error);
+}
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
@@ -46,11 +50,17 @@ const FormSchema = z.object({
  
   const amountInCents = amount * 100;
  
+ try {
   await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
+}
+catch (error) {
+  // We'll log the error to the console for now
+  console.error(error);
+}
  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
@@ -59,6 +69,8 @@ const FormSchema = z.object({
 //EDIT INVOICE
 
   export async function deleteInvoice(id: string) {
+    throw new Error('Failed to Delete Invoice');
+
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
 }
