b08cc422eed49b66886f8b0b3207aaed6eec918b
Paul Andrew Santos <paulandrewsantos@gmail.com>
2025-02-06 10:59:29 +0800

    Improving Accessibility
    Chapter 14

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -69,7 +69,6 @@ catch (error) {
 //EDIT INVOICE
 
   export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete Invoice');
 
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
